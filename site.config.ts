355ecc0ba74d5d4dc38b094ad431c7305df6e4c6
Kenneth <73374357+kaythio@users.noreply.github.com>
2023-11-28 13:48:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,7 +17,7 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  //twitter: 'null',
+  //twitter: null,
   github: 'kaythio',
   linkedin: 'kenneth-thio-uiux',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: :joy:"
+  defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
